7f033ce82288fc2a2cd4d82413115f522cd8a1cf
nicolasariza <nicolas_ariza@outlook.com>
2020-04-05 14:41:17 -0500

    Se crea estructura inicial del proyecto

---
+++ src/components/Formulario.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Formulario = () => {
+  return (
+    <form className="col-12">
+      <fieldset className="text-center">
+        <legend>Busca bebidas por categoría o ingrediente</legend>
+      </fieldset>
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            name="nombre"
+            className="form-control"
+            type="text"
+            placeholder="Buscar por ingrediente"
+          />
+        </div>
+        <div className="col-md-4">
+          <select className="form-control" name="categoria">
+            <option value="">--- Selecciona catergoría--</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            value="Buscar bebidas"
+            className="btn btn-block btn-primary"
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Formulario;
