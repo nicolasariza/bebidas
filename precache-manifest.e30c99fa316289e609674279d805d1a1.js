2f00220b9798050f34a1daa4ef5c431f48876903
nicolasariza <nicolas_ariza@outlook.com>
2020-04-05 21:42:13 -0500

    Updates

---
+++ precache-manifest.e30c99fa316289e609674279d805d1a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12be0124321622e8166ce7be4f4744c7",
+    "url": "/bebidas/index.html"
+  },
+  {
+    "revision": "9dc14ce46478b7ba9f98",
+    "url": "/bebidas/static/css/main.f12e8f9f.chunk.css"
+  },
+  {
+    "revision": "953a1409c956d2af8637",
+    "url": "/bebidas/static/js/2.17064f77.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/bebidas/static/js/2.17064f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc14ce46478b7ba9f98",
+    "url": "/bebidas/static/js/main.a36a3ca3.chunk.js"
+  },
+  {
+    "revision": "fbb9c03c5f1c9b09f54b",
+    "url": "/bebidas/static/js/runtime-main.37b2ca9d.js"
+  }
+]);
